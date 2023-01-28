b1becb51e0cd2a5befa4408cf5aa639961330da8
Tom Wilkinson <thomasmarkwilkinson@gmail.com>
2023-01-28 15:11:50 +0000

    testing images

--- src/controller.js
+++ src/controller.js
@@ -1,18 +1,29 @@
 (function exportController1() {
   class Controller1 {
-    constructor(player1) {
-      this.player1 = player1;
-      document.querySelector('#p1-attack').addEventListener('click', () => {
-        this.renderPlayer1();
-      });
-    }
-
+    constructor(name) {
+        this.name = name;
+        this.initialiseSea();
+      //this.player1 = player1;
+      //document.querySelector('#p1-attack').addEventListener('click', () => {
+       // console.log('click');
+       // this.renderPlayer1();
+       //   })
+        }
     renderPlayer1() {
-      const background = ['./images/bulbasaur.png'];
-      document.querySelector('#player1').style.backgroundImage = `url('${
-        background}')`;
+      document.querySelector('#player1').style.backgroundImage = "url('./images/test.png')";
     }
-  }
+    
+    initialiseSea() {
+        const backgrounds = ['/images/water0.png', '/images/water1.png'];
+        let backgroundIndex = 0;
+        window.setInterval(() => {
+          document.querySelector('#game').style.backgroundImage = `url('${
+            backgrounds[backgroundIndex % backgrounds.length]
+          }')`;
+          backgroundIndex += 1;
+        }, 1000);
+      }
+    }
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = Controller1;
   } else {
