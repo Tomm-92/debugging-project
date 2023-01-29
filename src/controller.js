118265051a35b6fdeab6b0a47bf307fd050da0e5
Tom Wilkinson <thomasmarkwilkinson@gmail.com>
2023-01-29 20:30:28 +0000

    player 1 animation

--- src/controller.js
+++ src/controller.js
@@ -2,7 +2,7 @@
   class Controller1 {
     constructor(name) {
       this.name = name;
-      //this.renderPlayer1();
+      this.renderPlayer1();
       // this.player1 = player1;
       // document.querySelector('#p1-attack').addEventListener('click', () => {
       // console.log('click');
@@ -15,32 +15,32 @@
     } */
 
     renderPlayer1() {  
-      const newPlayerElement1 = document.querySelector('#player1');
-      newPlayerElement1.setAttribute("src", "./css/images/charmander.png");
-    } 
+        const backgrounds = ['./css/images/char3.png', './css/images/char75.png'];
+        let backgroundIndex = 0;
+        window.setInterval(() => {
+          document.querySelector('#player1').style.content = `url('${
+            backgrounds[backgroundIndex % backgrounds.length]
+          }')`;
+          backgroundIndex += 1;
+        }, 750);
+      }
 
-    /*renderPlayer2() {
+    
+
+    renderPlayer2() {
         const newPlayerElement2 = document.createElement('img');
         newPlayerElement2.setAttribute("src", "./css/images/bulbasaur.png");
         newPlayerElement2.id = 'player2';
         const playerElement2 = document.querySelector('#game');
         playerElement2.appendChild(newPlayerElement2);
 
-    } */
+    } 
+
+
+  }
 
    
-    /*animatePlayer1() {
-      const backgrounds = ['./css/images/charmander.png', './css/images/bulbasaur.png'];
-      let backgroundIndex = 0;
-      window.setInterval(() => {
-        document.querySelector('#player1').setAttribute = `url('${
-          backgrounds[backgroundIndex % backgrounds.length]
-        }')`;
-        backgroundIndex += 1;
-      }, 1000);
-  } */
 
-}
 
 
   if (typeof module !== 'undefined' && module.exports) {
