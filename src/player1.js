476daaec46e39cb2f53ac5362a5f5086a41847b8
Tom Wilkinson <thomasmarkwilkinson@gmail.com>
2023-02-04 15:53:57 +0000

    extended player class now retrived from window

--- src/player1.js
+++ src/player1.js
@@ -1,6 +1,43 @@
-// eslint-disable-next-line babel/no-unused-expressions
+
+//const Character = require("./character1");
+
+/* class Character {
+  constructor(characterConfig) {
+    this.name = characterConfig.name;
+    this.health = characterConfig.health;
+    this.maxHealth = 10;
+    this.dialogue = characterConfig.dialogue;
+  }
+
+  _takeDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health < 0) {
+      this.health = 0;
+    }
+  }
+
+  _heal(potion) {
+    this.health = this.health + potion;
+    if (this.health > this.maxHealth) {
+      this.health = this.maxHealth;
+    }
+  }
+
+  speak() {
+    return this.dialogue;
+  }
+
+  get isAlive() {
+    return this.health > 0;
+  }
+} */
 
 (function exportPlayer() {
+  if (typeof module !== 'undefined' && module.exports) {
+    Character = require("./character1")
+  } else {
+    Character = window.Character
+  }
   class Player extends Character {
     constructor(characterConfig) {
       super(characterConfig);
@@ -26,7 +63,7 @@
     }
   }
   if (typeof module !== 'undefined' && module.exports) {
-    module.exports = Player;
+    module.exports = Player; 
   } else {
     window.Player = Player;
   }
